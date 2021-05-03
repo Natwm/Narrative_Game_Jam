0eef0650e889b42a7404b99b5d9b119fa625aec2
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2021-05-03 21:33:01 +0200

    Fix Caméra Parceque Nathan Code avec ses BOURSES

--- Assets/02_Scripts/GameHandle.cs
+++ Assets/02_Scripts/GameHandle.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class GameHandle : MonoBehaviour
 {
     public float moveAmount;
-    public float edge = 20;
+    public float edge;
 
     private Vector3 cameraPosition;
+    public Vector2 heightMinMax;
+    public Vector2 widthMinmax;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,27 +40,27 @@ public class GameHandle : MonoBehaviour
             Camera.main.transform.position = new Vector3(value, Camera.main.transform.position.y, Camera.main.transform.position.z);
         }
 
-        print(Input.mousePosition.y);
+        
         if(Input.mousePosition.x> Screen.width - edge)
         {
-            float value = Mathf.Clamp(Camera.main.transform.position.x - (moveAmount / 2) * Time.deltaTime, -0.6f, .7f);
+            float value = Mathf.Clamp(Camera.main.transform.position.x - (moveAmount / 2) * Time.deltaTime, heightMinMax.x, heightMinMax.y);
             Camera.main.transform.position = new Vector3(value, Camera.main.transform.position.y, Camera.main.transform.position.z);
         }
         if (Input.mousePosition.x < edge + 10)
         {
-            float value = Mathf.Clamp(Camera.main.transform.position.x + (moveAmount / 2) * Time.deltaTime, -0.6f, .7f);
+            float value = Mathf.Clamp(Camera.main.transform.position.x + (moveAmount / 2) * Time.deltaTime, heightMinMax.x, heightMinMax.y);
             Camera.main.transform.position = new Vector3(value, Camera.main.transform.position.y, Camera.main.transform.position.z);
         }
 
         if (Input.mousePosition.y > Screen.height - edge)
         {
-            float value = Mathf.Clamp(Camera.main.transform.position.z - moveAmount * Time.deltaTime, -2.5f, 2.3f);
+            float value = Mathf.Clamp(Camera.main.transform.position.z - moveAmount * Time.deltaTime, widthMinmax.x, widthMinmax.y);
             Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, value);
         }
         
         if (Input.mousePosition.y < edge)
         {
-            float value = Mathf.Clamp(Camera.main.transform.position.z + moveAmount * Time.deltaTime, -2.5f, 2.3f);
+            float value = Mathf.Clamp(Camera.main.transform.position.z + moveAmount * Time.deltaTime, widthMinmax.x, widthMinmax.y);
             Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, value);
         }
     }
