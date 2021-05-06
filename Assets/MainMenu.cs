34ab01e90e7dec907e4f91c13c7cfbe9e67ccb81
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2021-05-06 22:55:34 +0200

    PetiteTransitionPasPiqueeDesHannetons

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,15 +5,53 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
 using PixelCrushers.DialogueSystem;
+using FMODUnity;
 
 public class MainMenu : MonoBehaviour
 {
     public Image CreditImage;
+    public Image ParentImage;
+    public Image TitleImage;
+
+    public Button[] fadeUi;
+
+    public Transform startPoint;
+    public Transform endPoint;
+
     public Sprite[] soundsprite;
     bool soundState;
     public Text startText;
     public Text creditText;
     public Text exitText;
+
+    [FMODUnity.EventRef]
+    public string MusicMenu;
+
+    FMOD.Studio.EventInstance EventMenu;
+
+    public void PetiteTransitionPasPiquéeDesHannetons()
+    {
+        EventMenu = FMODUnity.RuntimeManager.CreateInstance(MusicMenu); 
+        ParentImage.rectTransform.DOMove(endPoint.position,5f);
+        ParentImage.DOFade(255,3000);
+        foreach (Button item in fadeUi)
+        {
+            item.image.DOFade(255,3000);
+            Text tempText;
+            item.transform.GetChild(0).TryGetComponent<Text>(out tempText);
+            if (tempText != null)
+            {
+               tempText.DOFade(255, 3000);
+            }
+        }
+        Invoke("LaunchTitle", 1);
+    }
+
+    public void LaunchTitle()
+    {
+        EventMenu.start();
+        TitleImage.DOFade(255, 2000);
+    }
 
     //Launch Scene
     public void LaunchGame()
@@ -95,12 +133,16 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Master = FMODUnity.RuntimeManager.GetBus("bus:/"); 
+        Master = FMODUnity.RuntimeManager.GetBus("bus:/");
+        ParentImage.rectTransform.SetPositionAndRotation(startPoint.transform.position, ParentImage.rectTransform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            PetiteTransitionPasPiquéeDesHannetons();
+        }
     }
 }
